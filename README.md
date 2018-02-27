faa62381f24314e95b6fffa26a95b4d5b441baa4
tonkytonky <as@asamsonov.me>
2018-02-27 19:57:43 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+This is the script for searching midnight commiters at educational platform
+[DEVMAN.org](https://devman.org). It scans over the platforms API and searches
+for the people who committed thier exercises from midnight till 9 AM their 
+local time.
+
+
+# Usage
+
+To run the script you need Python 3.5 or higher to be installed. After 
+launching it outputs to console all the midnighters names. 
+
+Example: 
+```bash
+$ python seek_dev_nighters.py
+<name1>
+<name2>
+<name3>
+...
+```
+
 
 # Project Goals
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+The code is written for educational purposes. 
+Training course for web-developers - [DEVMAN.org](https://devman.org)
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,66 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+from datetime import datetime
+import json
+import requests
 
-def get_midnighters():
-    pass
+from pytz import utc, timezone
+
+
+def _main():
+    midnighters = get_midnighters(load_attempts(load_number_of_pages()))
+    for midnighter in sorted(midnighters):
+        print(midnighter)
+
+
+def load_attempts(number_of_pages):
+    for page_num in range(number_of_pages):
+        api_page_num = page_num + 1
+        for record in json.loads(load_attempts_page(api_page_num))['records']:
+            yield {
+                'username': record['username'],
+                'timestamp': record['timestamp'],
+                'timezone': record['timezone'],
+            }
+
+
+def load_number_of_pages():
+    return int(json.loads(load_attempts_page(1))['number_of_pages'])
+
+
+def load_attempts_page(page):
+    return requests.get(
+        'https://devman.org/api/challenges/solution_attempts/',
+        params={'page': page}
+    ).text
+
+
+def get_midnighters(attempts):
+    midnighters = set()
+    for attempt in attempts:
+        utc_dt = utc.localize(datetime.utcfromtimestamp(attempt['timestamp']))
+        local_tz = timezone(attempt['timezone'])
+        local_dt = utc_dt.astimezone(local_tz)
+        local_date = {
+            'year': local_dt.year,
+            'month': local_dt.month,
+            'day': local_dt.day,
+        }
+        midnight = local_tz.localize(datetime(
+            **local_date,
+            hour=0,
+            minute=0,
+            second=0
+        ))
+        working_hours_start = local_tz.localize(datetime(
+            **local_date,
+            hour=9,
+            minute=0,
+            second=0
+        ))
+        if midnight < local_dt < working_hours_start:
+            midnighters.add(attempt['username'])
+
+    return list(midnighters)
+
 
 if __name__ == '__main__':
-  pass
+    _main()
